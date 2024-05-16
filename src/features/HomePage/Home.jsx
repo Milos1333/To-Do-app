764c4b6e974f8f1cea6e0b3d5396b64bf18d2c40
Milos1333 <milosklepic91@gmail.com>
2024-05-16 15:55:37 +0200

    feat-createtask

--- src/features/HomePage/Home.jsx
+++ src/features/HomePage/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
   const handleNavigate = () => {
     navigate("../createtask");
   };
+
   return (
     <div className="main-home">
       <div className="container-home">
         <h2>What are you planning for today?</h2>
         <h2>Create your new task!</h2>
         <MyButton
-          color="white"
           textColor="black"
           buttonText="Create"
           handleToButton={handleNavigate}
